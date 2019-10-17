553523f19ebbefc744d6b91b65f4cae29faa5efd
izzad24 <afifizzad_mazlan@dell.com>
2019-10-17 20:40:42 +0800

    create auth service, module, interface

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    IonicStorageModule
+
+  ]
+})
+export class AuthModule { }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { tap } from  'rxjs/operators';
+import { Observable, BehaviorSubject } from  'rxjs';
+
+import { Storage } from  '@ionic/storage';
+import { User } from  './user';
+import { AuthResponse } from  './auth-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  AUTH_SERVER_ADDRESS:  string  =  'http://localhost:3000';
+  authSubject  =  new  BehaviorSubject(false);
+
+  constructor(private  httpClient:  HttpClient, private  storage:  Storage) { }
+
+  register(user: User): Observable<AuthResponse> {
+    return this.httpClient.post<AuthResponse>(`${this.AUTH_SERVER_ADDRESS}/register`, user).pipe(
+      tap(async (res:  AuthResponse ) => {
+
+        if (res.user) {
+          await this.storage.set("ACCESS_TOKEN", res.user.access_token);
+          await this.storage.set("EXPIRES_IN", res.user.expires_in);
+          this.authSubject.next(true);
+        }
+      })
+
+    );
+  }
+}
